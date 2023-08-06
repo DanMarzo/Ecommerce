ebca6e01700c050c5314098cfc15d7e59d7b7d6c
DanMarzo <marzogildan@gmail.com>
2023-08-05 23:49:52 -0300

    Instalando Packages e iniciando os Models

---
+++ Ecommerce.Domain/ProductStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ecommerce.Domain;
+
+public enum ProductStatus
+{
+    [EnumMember(Value = "Produto Inativo")]
+    Inativo = 1,
+    [EnumMember(Value = "Produto Ativo")]
+    Ativo = 2
+}
---
+++ Ecommerce.Domain/Produto.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Domain.Common;
+
+namespace Ecommerce.Domain;
+
+public class Produto : BaseDomainModel
+{
+    public string? Nome { get; set; }
+    public decimal Preco { get; set; }
+    public string? Descricao { get; set; }
+    public string? Vendedor { get; set; }
+    public int Stock { get; set; }
+    public ProductStatus Status { get; set; }
+    public int CategoryId { get; set; }
+}
