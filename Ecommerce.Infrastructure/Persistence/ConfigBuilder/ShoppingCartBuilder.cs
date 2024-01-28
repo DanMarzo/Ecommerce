eaf8583b097016fc6a7a9743f36f5e4eb6fff070
DanMarzo <marzogildan@gmail.com>
2024-01-28 11:25:23 -0300

    Relacionamento de ShoppingCart com shoppingCartItems

---
+++ Ecommerce.Infrastructure/Persistence/ConfigBuilder/ShoppingCartBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Persistence.ConfigBuilder;
+
+public class ShoppingCartBuilder : IEntityTypeConfiguration<ShoppingCart>
+{
+    public void Configure(EntityTypeBuilder<ShoppingCart> builder)
+    {
+        builder
+            .HasMany(p => p.ShoppingCartItems)
+            .WithOne(p => p.ShoppingCart)
+            .HasForeignKey(p => p.ShoppingCartId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
