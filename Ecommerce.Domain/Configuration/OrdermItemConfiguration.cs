1733a1ed88e386be3d9c5c6f21c6bf5040b18e85
DanMarzo <marzogildan@gmail.com>
2024-01-28 15:48:42 -0300

    Incluindo packages

---
+++ Ecommerce.Domain/Configuration/OrdermItemConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Domain.Configuration;
+
+public class OrdermItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.Property(x => x.Preco)
+            .HasColumnType("decimal(10,2)");
+    }
+}
