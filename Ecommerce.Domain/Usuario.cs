3a2b1d78401b1bafd519b1a83c82295a251dd926
DanMarzo <marzogildan@gmail.com>
2024-01-27 22:00:40 -0300

    Atualizando package e incluindo roles e usuario

---
+++ Ecommerce.Domain/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Ecommerce.Domain;
+
+public class Usuario : IdentityUser
+{
+    public String? Nome { get; set; }
+    public String? Sobrenome { get; set; }
+    public String? Telefone { get; set; }
+    public String? AvatarURL { get; set; }
+    public bool IsActive { get; set; }
+}
