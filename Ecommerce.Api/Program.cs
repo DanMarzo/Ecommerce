d2d5389b259ae728582f62ef76fee5cfba8c4815
DanMarzo <marzogildan@gmail.com>
2024-01-29 22:22:20 -0300

    Implementando regras de jwt e regra de cors

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,11 +1,70 @@
+using Ecommerce.Domain;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<EcommerceDbContext>(
+    options => options.UseMySQL(
+        builder.Configuration.GetConnectionString("default") ?? "",
+        //No console ele imprime todas as tarefas em andamento
+        b => b.MigrationsAssembly(typeof(EcommerceDbContext).Assembly.FullName)
+    ));
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
+
+//Configurando Identity
+IdentityBuilder identityBuilder = builder.Services.AddIdentityCore<Usuario>();
+
+identityBuilder = new IdentityBuilder(identityBuilder.UserType, identityBuilder.Services);
+//Acessivel para o payload do token  - AddDefaultTokenProviders();
+identityBuilder.AddRoles<IdentityRole>().AddDefaultTokenProviders();
+identityBuilder.AddClaimsPrincipalFactory<UserClaimsPrincipalFactory<Usuario, IdentityRole>>();
+identityBuilder.AddEntityFrameworkStores<EcommerceDbContext>();
+identityBuilder.AddSignInManager<SignInManager<Usuario>>();
+
+builder.Services.TryAddSingleton<ISystemClock, SystemClock>();
+
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:key"]!));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters { 
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =  key,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+        };
+    });
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", 
+        builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
@@ -16,8 +75,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
+app.UseAuthentication();
 
+app.UseAuthorization();
+app.UseCors("CorsPolicy");
 app.MapControllers();
 
 app.Run();
