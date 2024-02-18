68af6cebb7aa856e88dfdec4b8ec6c89541510ea
DanMarzo <marzogildan@gmail.com>
2024-02-18 19:37:07 -0300

    Inclusao de appsettings e modelos e services para upload no cloudinary

---
+++ Ecommerce.Infrastructure/ImageCloudinary/ManageImageService.cs
@@ -0,0 +1,43 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Ecommerce.Application.Contracts.Infrastructure;
+using Ecommerce.Application.Models.ImageManagement;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Options;
+
+namespace Ecommerce.Infrastructure.ImageCloudinary;
+
+public class ManageImageService : IManageImageService
+{
+    public CloudinarySettings _cloudinarySettings { get; set; }
+    public ManageImageService(IOptions<CloudinarySettings> cloudinarySettings) => _cloudinarySettings = cloudinarySettings.Value;
+    public async Task<ImageResponse> UploadImage(ImageData imageStream)
+    {
+
+        //Cloud = CloudinaryConfiguration.CloudName;
+        //ApiKey = CloudinaryConfiguration.ApiKey;
+        //ApiSecret = CloudinaryConfiguration.ApiSecret;
+        Account account = new Account(
+            _cloudinarySettings.CloudName,
+            _cloudinarySettings.ApiKey,
+            _cloudinarySettings.ApiSecret
+            );
+
+        var cloudinary = new Cloudinary(account);
+        var uploadImage = new ImageUploadParams()
+        {
+            File = new FileDescription(imageStream.Nome, imageStream.ImageStream)
+        };
+        var uploadResult = await cloudinary.UploadAsync(uploadImage);
+
+        if (uploadResult.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            return new ImageResponse
+            {
+                PublicId = uploadResult.PublicId,
+                Url = uploadResult.Url.ToString()
+            };
+        }
+        throw new Exception("Nao foi possivel guardar a imagem");
+    }
+}
