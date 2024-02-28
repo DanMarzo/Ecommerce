7313947e3bf663e2ab30b21dbb15e07973501c58
DanMarzo <marzogildan@gmail.com>
2024-02-27 23:02:44 -0300

    Gerando midlewares

---
+++ Ecommerce.Api/Errors/CodeErrorException.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ecommerce.Api.Errors;
+
+public class CodeErrorException : CodeErrorResponse
+{
+    public CodeErrorException(
+        int statusCode, string[]? message = null, 
+        string? details = null) 
+        : base(statusCode, message)
+    {
+        Details = details;
+    }
+
+    [JsonPropertyName("details")]
+    public string? Details { get; set; }
+
+}
---
+++ Ecommerce.Api/Errors/CodeErrorResponse.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ecommerce.Api.Errors;
+
+public class CodeErrorResponse
+{
+    [JsonPropertyName("statusCode")]
+    public int StatusCode { get; set; }
+    [JsonPropertyName("message")]
+    public string[]? Message { get; set; }
+
+    public CodeErrorResponse(int statusCode, string[]? message = null)
+    {
+        StatusCode = statusCode;
+        if (message is null)
+        {
+            Message = [];
+            var text = GetDefaultMessageStatusCode(statusCode);
+            Message[0] = text;
+        }
+        else
+        {
+            Message = message;
+        }
+    }
+
+    private string GetDefaultMessageStatusCode(int statusCode)
+    {
+        return statusCode switch
+        {
+            400 => "A requisicao enviada contem erros",
+            401 => "Voce nao possui autorizacao para usar este recurso",
+            404 => "O recurso nao foi localizado",
+            500 => "Ocorreram erros no servidor",
+            _ => string.Empty,
+        };
+    }
+
+
+}
