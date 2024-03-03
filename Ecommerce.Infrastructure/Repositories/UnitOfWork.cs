2fb63e8ad62f6e8b85d5e3d8896ed5f802134dcc
DanMarzo <marzogildan@gmail.com>
2024-03-03 18:59:37 -0300

    Excluindo comentario

--- Ecommerce.Infrastructure/Repositories/UnitOfWork.cs
+++ Ecommerce.Infrastructure/Repositories/UnitOfWork.cs
@@ -32,22 +32,6 @@ public class UnitOfWork : IUnitOfWork
 
     public IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : class
     {
-        //if (_repositories is null)
-        //{
-        //    _repositories = new Hashtable();
-        //}
-
-        //var type = typeof(TEntity).Name;
-
-        //if (!_repositories.ContainsKey(type))
-        //{
-        //    var repositoryType = typeof(RepositoryBase<>);
-        //    var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context);
-        //    _repositories.Add(type, repositoryInstance);
-        //}
-
-        //return (IAsyncRepository<TEntity>)_repositories[type]!;
-
         if (_repositories is null) _repositories = new Hashtable();
 
         var type = typeof(TEntity).Name;
