b793c659f269e82e7ce98ce66b1868392e4fbf8c
DanMarzo <marzogildan@gmail.com>
2024-03-03 20:26:04 -0300

    Incluindo ViewModels

---
+++ Ecommerce.Application/Features/Images/Queries/Vms/ImageVm.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Application.Features.Images.Queries.Vms;
+
+public class ImageVm
+{
+    public int Id { get; set; }
+    public string? Uri { get; set; }
+    public int ProductId { get; set; }
+    public string? PublicCoDE { get; set; }
+}
